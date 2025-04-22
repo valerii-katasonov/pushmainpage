1557bbc43773565f365094607762ff9669f29464
valerii-katasonov <valerii.katasonov@gmail.com>
2025-04-22 18:24:17 +0200

    Update schedule-class-2b.js

--- schedule-class-2b.js
+++ schedule-class-2b.js
@@ -29,7 +29,7 @@ const schedule = {
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Читання", pl: "Czytanie" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Образотворче мистецтво", pl: "Plastyka" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Plastyka", pl: "Plastyka" }, time: "13:00 - 13:45" }
   ],
   Wednesday: [
     { number: 1, subject: { ua: "Читання", pl: "Czytanie" }, time: "9:00 - 9:45" },
@@ -40,7 +40,7 @@ const schedule = {
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Природознавство", pl: "Przyroda" }, time: "13:00 - 13:45" },
+    { number: 5, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "13:45 - 14:30" }
   ],
   Thursday: [
@@ -52,14 +52,14 @@ const schedule = {
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Природознавство", pl: "Przyroda" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "13:00 - 13:45" }
   ],
   Friday: [
     { number: 1, subject: { ua: "Польська мова", pl: "Język polski" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
     { number: 2, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
-    { number: 3, subject: { ua: "Образотворче мистецтво", pl: "Plastyka" }, time: "10:50 - 11:35" },
+    { number: 3, subject: { ua: "Plastyka", pl: "Plastyka" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: " ", subject: { ua: "Очікування автобуса", pl: "Oczekiwanie na autobus" }, time: "11:55 - 12:20" },
     { number: " ", subject: { ua: "Шлях зі школи до басейну на автобусі", pl: "Droga ze szkoły na basen autobusem" }, time: "12:20 - 12:45" },
