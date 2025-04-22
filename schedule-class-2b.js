a5655bdf59ae01c1cc25bf0e1287b0f70951cee0
valerii-katasonov <valerii.katasonov@gmail.com>
2025-04-22 18:00:25 +0200

    Update schedule-class-2b.js

--- schedule-class-2b.js
+++ schedule-class-2b.js
@@ -14,7 +14,7 @@ const schedule = {
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
     { number: 2, subject: { ua: "Інформатика", pl: "Informatyka" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
-    { number: 3, subject: { ua: "Ранкова освіта", pl: "Edukacja wczesnoszkolna" }, time: "10:50 - 11:35" },
+    { number: 3, subject: { ua: "Edukacja wczesnoszkolna", pl: "Edukacja wczesnoszkolna" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Математика", pl: "Matematyka" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
