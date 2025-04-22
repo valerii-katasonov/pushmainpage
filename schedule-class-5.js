d8de4d527ab1fde402c4b2f7dc38137aa071c04b
valerii-katasonov <valerii.katasonov@gmail.com>
2025-04-22 20:35:05 +0200

    Create schedule-class-5.js

---
+++ schedule-class-5.js
@@ -0,0 +1,72 @@
+const dayNames = {
+  Monday: { ua: "Понеділок", pl: "Poniedziałek" },
+  Tuesday: { ua: "Вівторок", pl: "Wtorek" },
+  Wednesday: { ua: "Середа", pl: "Środa" },
+  Thursday: { ua: "Четвер", pl: "Czwartek" },
+  Friday: { ua: "П'ятниця", pl: "Piątek" },
+  Saturday: { ua: "Субота", pl: "Sobota" },
+  Sunday: { ua: "Неділя", pl: "Niedziela" }
+};
+
+const schedule = {
+  Monday: [
+    { number: 1, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:00 - 9:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
+    { number: 2, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "9:55 - 10:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
+    { number: 3, subject: { ua: "Історія та суспільство", pl: "Historia i społeczeństwo" }, time: "10:50 - 11:35" },
+    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
+    { number: 4, subject: { ua: "Українська та світова література", pl: "Literatura ukraińska i światowa" }, time: "11:55 - 12:40" },
+    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
+    { number: 5, subject: { ua: "Польська мова", pl: "Język polski" }, time: "13:00 - 13:45" }
+  ],
+  Tuesday: [
+    { number: 1, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:00 - 9:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
+    { number: 2, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:55 - 10:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
+    { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
+    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
+    { number: 4, subject: { ua: "Польська мова", pl: "Język polski" }, time: "11:55 - 12:40" },
+    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
+    { number: 5, subject: { ua: "Інформатика", pl: "Informatyka" }, time: "13:00 - 13:45" }
+  ],
+  Wednesday: [
+    { number: 1, subject: { ua: "Plastyka", pl: "Plastyka" }, time: "9:00 - 9:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
+    { number: 2, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:55 - 10:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
+    { number: 3, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "10:50 - 11:35" },
+    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
+    { number: 4, subject: { ua: "Українська та світова література", pl: "Literatura ukraińska i światowa" }, time: "11:55 - 12:40" },
+    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
+    { number: 5, subject: { ua: "Математика", pl: "Matematyka" }, time: "13:00 - 13:45" }
+  ],
+  Thursday: [
+    { number: 1, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:00 - 9:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
+    { number: 2, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "9:55 - 10:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
+    { number: 3, subject: { ua: "Польська мова", pl: "Język polski" }, time: "10:50 - 11:35" },
+    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
+    { number: 4, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "11:55 - 12:40" },
+    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" }
+  ],
+  Friday: [
+    { number: 1, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:00 - 9:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
+    { number: 2, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "9:55 - 10:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
+    { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
+    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
+    { number: 4, subject: { ua: "Техніка", pl: "Technika" }, time: "11:55 - 12:40" },
+    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
+    { number: " ", subject: { ua: "Очікування автобуса", pl: "Oczekiwanie na autobus" }, time: "13:00 - 13:20" },
+    { number: " ", subject: { ua: "Шлях зі школи до басейну на автобусі", pl: "Droga ze szkoły na basen autobusem" }, time: "13:20 - 13:45" },
+    { number: 5, subject: { ua: "Фізичне виховання (басейн)", pl: "Wychowanie fizyczne (basen)" }, time: "14:00 - 15:00" },
+    { number: " ", subject: { ua: "Шлях з басейну до школи на автобусі", pl: "Droga z basenu do szkoły autobusem" }, time: "15:30 - 16:00" }
+  ]
+};
+
+window.schedule = schedule;
+window.dayNames = dayNames;
