c48857a712aa78d6ab1156d88936c35b859f9cb2
devilgate364 <80877597+devilgate364@users.noreply.github.com>
2025-04-24 15:08:37 +0200

    Update schedule-class-5.js

--- schedule-class-5.js
+++ schedule-class-5.js
@@ -16,9 +16,9 @@ const schedule = {
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Історія та суспільство", pl: "Historia i społeczeństwo" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
-    { number: 4, subject: { ua: "Українська та світова література", pl: "Literatura ukraińska i światowa" }, time: "11:55 - 12:40" },
+    { number: 4, subject: { ua: "Фізичне виховання", pl: "Wychowanie fizyczne" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Польська мова", pl: "Język polski" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Фізичне виховання", pl: "Wychowanie fizyczne" }, time: "13:00 - 13:45" }
   ],
   Tuesday: [
     { number: 1, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:00 - 9:45" },
@@ -38,19 +38,22 @@ const schedule = {
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
-    { number: 4, subject: { ua: "Українська та світова література", pl: "Literatura ukraińska i światowa" }, time: "11:55 - 12:40" },
+    { number: 4, subject: { ua: "Польська мова", pl: "Język polski" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Математика", pl: "Matematyka" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Математика", pl: "Matematyka" }, time: "13:00 - 13:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
+    { number: 6, subject: { ua: "Українська та світова література", pl: "Literatura ukraińska i światowa" }, time: "13:55 - 14:40" }
   ],
   Thursday: [
-    { number: 1, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:00 - 9:45" },
+    { number: 1, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
     { number: 2, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
-    { number: 3, subject: { ua: "Польська мова", pl: "Język polski" }, time: "10:50 - 11:35" },
+    { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "11:55 - 12:40" },
-    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" }
+    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
+    { number: 5, subject: { ua: "Польська мова", pl: "Język polski" }, time: "10:50 - 11:35" }
   ],
   Friday: [
     { number: 1, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:00 - 9:45" },
