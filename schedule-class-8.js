690b9f718eeef3692f36bc12bcc28e32e737da3b
devilgate364 <80877597+devilgate364@users.noreply.github.com>
2025-04-29 17:24:13 +0200

    Update schedule-class-8.js

--- schedule-class-8.js
+++ schedule-class-8.js
@@ -10,7 +10,9 @@ const schedule = {
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Польська мова", pl: "Język polski" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
-    { number: 6, subject: { ua: "Біологія", pl: "Biologia" }, time: "13:55 - 14:40" }
+    { number: 6, subject: { ua: "Хімія", pl: "Chemia" }, time: "13:55 - 14:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "14:40 - 14:50" },
+    { number: 7, subject: { ua: "Біологія", pl: "Biologia" }, time: "14:50 - 15:35" }
   ],
   "Tuesday": [
     { number: 1, subject: { ua: "Алгебра", pl: "Algebra" }, time: "9:00 - 9:45" },
@@ -23,12 +25,14 @@ const schedule = {
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Геометрія", pl: "Geometria" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
-    { number: 6, subject: { ua: "Хімія", pl: "Chemia" }, time: "13:55 - 14:40" }
+    { number: 6, subject: { ua: "Фізична культура", pl: "Wychowanie fizyczne" }, time: "13:55 - 14:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "14:40 - 14:50" },
+    { number: 7, subject: { ua: "Фізична культура", pl: "Wychowanie fizyczne" }, time: "14:50 - 15:35" }
   ],
   "Wednesday": [
     { number: 1, subject: { ua: "Географія (термінологія)", pl: "Geografia (terminologia)" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
-    { number: 2, subject: { ua: "Геометрія", pl: "Geometria" }, time: "9:55 - 10:40" },
+    { number: 2, subject: { ua: "Фізика", pl: "Fizyka" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Інформатика", pl: "Informatyka" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід", pl: "Obiad" }, time: "11:35 - 11:55" },
@@ -36,7 +40,7 @@ const schedule = {
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Польська мова", pl: "Język polski" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
-    { number: 6, subject: { ua: "Фізика", pl: "Fizyka" }, time: "13:55 - 14:40" },
+    { number: 6, subject: { ua: "Геометрія", pl: "Geometria" }, time: "13:55 - 14:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "14:40 - 14:50" },
     { number: 7, subject: { ua: "Біологія", pl: "Biologia" }, time: "14:50 - 15:35" }
   ],
@@ -49,22 +53,24 @@ const schedule = {
     { number: " ", subject: { ua: "Обід", pl: "Obiad" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Українська література", pl: "Literatura ukraińska" }, time: "13:00 - 13:45" },
+    { number: 5, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
     { number: 6, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "13:55 - 14:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "14:40 - 14:50" },
-    { number: 7, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "14:50 - 15:35" }
+    { number: 7, subject: { ua: "Географія (польською)", pl: "Geografia (Polska)" }, time: "14:50 - 15:35" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "15:35 - 15:45" },
+    { number: 7, subject: { ua: "Географія (польською)", pl: "Geografia (Polska)" }, time: "15:45 - 16:30" }
   ],
   "Friday": [
-    { number: 1, subject: { ua: "Українська література", pl: "Literatura ukraińska" }, time: "9:00 - 9:45" },
+    { number: 1, subject: { ua: "Польська мова", pl: "Język polski" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
-    { number: 2, subject: { ua: "Польська мова", pl: "Język polski" }, time: "9:55 - 10:40" },
+    { number: 2, subject: { ua: "Українська література", pl: "Literatura ukraińska" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Хімія", pl: "Chemia" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід", pl: "Obiad" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Всесвітня історія", pl: "Historia świata" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Історія України", pl: "Historia Ukrainy" }, time: "13:00 - 13:45" },
+    { number: 5, subject: { ua: "Всесвітня історія", pl: "Historia świata" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Очікування автобуса", pl: "Oczekiwanie na autobus" }, time: "13:45 - 14:20" },
     { number: " ", subject: { ua: "Шлях зі школи на басейн автобусом", pl: "Droga ze szkoły na basen autobusem" }, time: "14:20 - 14:45" },
     { number: 6, subject: { ua: "Фізичне виховання (басейн)", pl: "Wychowanie fizyczne (basen)" }, time: "15:00 - 16:00" },
