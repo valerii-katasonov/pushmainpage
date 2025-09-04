298f84d6f2a7458fa0b11932d79ce6416fbc1993
valerii-katasonov <valerii.katasonov@gmail.com>
2025-09-04 21:41:00 +0200

    Update schedule-class-6.js

--- schedule-class-6.js
+++ schedule-class-6.js
@@ -47,7 +47,7 @@ const schedule = {
     { number: " ", subject: { ua: "Обід класів 1-4", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Польська мова", pl: "Język polski" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід класів 5-6", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 6, subject: { ua: "Історія та суспільство", pl: "Historia i społeczeństwo" }, time: "13:00 - 13:45" },
+    { number: 5, subject: { ua: "Історія та суспільство", pl: "Historia i społeczeństwo" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
     { number: 6, subject: { ua: "Біологія", pl: "Biologia" }, time: "13:55 - 14:40" }
   ],
