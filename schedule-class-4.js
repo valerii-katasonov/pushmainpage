9c6e741443c32635c8b775b1e2a19b7cdaf3dd64
valerii-katasonov <valerii.katasonov@gmail.com>
2025-09-05 16:21:16 +0200

    Update schedule-class-4.js

--- schedule-class-4.js
+++ schedule-class-4.js
@@ -10,15 +10,15 @@ const dayNames = {
 
 const schedule = {
   Monday: [
-    { number: 1, subject: { ua: "Читання", pl: "Czytanie" }, time: "9:00 - 9:45" },
+    { number: 1, subject: { ua: "Edukacja wczesnoszkolna", pl: "Edukacja wczesnoszkolna" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
-    { number: 2, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "9:55 - 10:40" },
+    { number: 2, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
-    { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
+    { number: 3, subject: { ua: "Інформатика", pl: "Informatyka" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
-    { number: 4, subject: { ua: "Польська мова", pl: "Język polski" }, time: "11:55 - 12:40" },
+    { number: 4, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Класна година", pl: "Godzina wychowawcza" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "13:00 - 13:45" }
   ],
   Tuesday: [
     { number: 1, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "9:00 - 9:45" },
@@ -29,7 +29,7 @@ const schedule = {
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Edukacja wczesnoszkolna", pl: "Edukacja wczesnoszkolna" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Plastyka", pl: "Plastyka" }, time: "13:00 - 13:45" },
+    { number: 5, subject: { ua: "Plastyka/Tehnika", pl: "Plastyka/Tehnika" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
     { number: 6, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "13:55 - 14:40" }
   ],
@@ -44,18 +44,18 @@ const schedule = {
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Читання", pl: "Czytanie" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
-    { number: 6, subject: { ua: "Фізична культура", pl: "Wychowanie fizyczne" }, time: "13:55 - 14:40" }
+    { number: 6, subject: { ua: "Фізична культура/Музика", pl: "Wychowanie fizyczne/Muz" }, time: "13:55 - 14:40" }
   ],
   Thursday: [
-    { number: 1, subject: { ua: "Польська мова", pl: "Język polski" }, time: "9:00 - 9:45" },
+    { number: 1, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
-    { number: 2, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "9:55 - 10:40" },
+    { number: 2, subject: { ua: "Edukacja wczesnoszkolna", pl: "Edukacja wczesnoszkolna" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
-    { number: 4, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "11:55 - 12:40" },
+    { number: 4, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Читання", pl: "Czytanie" }, time: "13:00 - 13:45" }
   ],
   Friday: [
     { number: 1, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:00 - 9:45" },
