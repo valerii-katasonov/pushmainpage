777675ab55e5acfd0806b9ce0138e2e0909a1d0b
VikaVikaK <112518114+VikaVikaK@users.noreply.github.com>
2025-09-11 10:24:38 +0300

    Update schedule-class-3.js

--- schedule-class-3.js
+++ schedule-class-3.js
@@ -38,13 +38,15 @@ const schedule = {
   Wednesday: [
     { number: 1, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
-    { number: 2, subject: { ua: "Польська мова", pl: "Język polski" }, time: "9:55 - 10:40" },
+    { number: 2, subject: { ua: "Едукація", pl: "Edukacja" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
-    { number: 4, subject: { ua: "Інформатика", pl: "Informatyka" }, time: "11:55 - 12:40" },
+    { number: 4, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
-    { number: 5, subject: { ua: "Читання", pl: "Czytanie" }, time: "13:00 - 13:45" }
+    { number: 5, subject: { ua: "Читання", pl: "Czytanie" }, time: "13:00 - 13:45" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
+    { number: 6, subject: { ua: "Plastyka / Technika", pl: "Plastyka / Technika" }, time: "13:55 - 14:40" }
   ],
   Thursday: [
     { number: 1, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:00 - 9:45" },
