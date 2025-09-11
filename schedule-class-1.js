27a7576fbcd7e1ee22318e26b55ba61c6e90d4b7
valerii-katasonov <valerii.katasonov@gmail.com>
2025-09-11 12:42:29 +0200

    Update schedule-class-1.js

--- schedule-class-1.js
+++ schedule-class-1.js
@@ -15,9 +15,9 @@ const schedule = {
     { number: 2, subject: { ua: "Навчання грамоти", pl: "Nauka czytania i pisania" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "10:50 - 11:35" },
-    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
+    { number: " ", subject: { ua: "Обід 1-3 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Математика", pl: "Matematyka" }, time: "11:55 - 12:40" },
-    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
+    { number: " ", subject: { ua: "Обід 4-5 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Я досліджую світ", pl: "Odkrywam świat" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
     { number: 6, subject: { ua: "Музичне мистецтво / Виховання фізичне", pl: "Sztuka muzyczna / Wychowanie fizyczne" }, time: "13:55 - 14:40" }
@@ -28,9 +28,9 @@ const schedule = {
     { number: 2, subject: { ua: "Навчання грамоти", pl: "Nauka czytania i pisania" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Навчання грамоти", pl: "Nauka czytania i pisania" }, time: "10:50 - 11:35" },
-    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
+    { number: " ", subject: { ua: "Обід 1-3 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Математика", pl: "Matematyka" }, time: "11:55 - 12:40" },
-    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
+    { number: " ", subject: { ua: "Обід 4-5 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Польська мова", pl: "Język Polski" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
     { number: 6, subject: { ua: "Plastyka", pl: "Plastyka" }, time: "13:55 - 14:40" }
@@ -41,9 +41,9 @@ const schedule = {
     { number: 2, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Математика", pl: "Matematyka" }, time: "10:50 - 11:35" },
-    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
+    { number: " ", subject: { ua: "Обід 1-3 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Навчання грамоти", pl: "Nauka czytania i pisania" }, time: "11:55 - 12:40" },
-    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
+    { number: " ", subject: { ua: "Обід 4-5 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Навчання грамоти", pl: "Nauka czytania i pisania" }, time: "13:00 - 13:45" }
   ],
   Thursday: [
@@ -52,9 +52,9 @@ const schedule = {
     { number: 2, subject: { ua: "Математика", pl: "Matematyka" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "10:50 - 11:35" },
-    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
+    { number: " ", subject: { ua: "Обід 1-3 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
     { number: 4, subject: { ua: "Навчання грамоти", pl: "Nauka czytania i pisania" }, time: "11:55 - 12:40" },
-    { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
+    { number: " ", subject: { ua: "Обід 4-5 класи", pl: "Obiad dla klas 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Technika", pl: "Technika" }, time: "13:00 - 13:45" }
   ],
   Friday: [
@@ -63,7 +63,7 @@ const schedule = {
     { number: 2, subject: { ua: "Польська мова", pl: "Język Polski" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Я досліджую світ", pl: "Odkrywam świat" }, time: "10:50 - 11:35" },
-    { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
+    { number: " ", subject: { ua: "Обід 1-3 класи", pl: "Obiad dla klas 1-4" }, time: "11:35 - 11:55" },
     { number: " ", subject: { ua: "Очікування автобусу", pl: "Oczekiwanie na autobus" }, time: "11:55 - 12:20" },
     { number: " ", subject: { ua: "Шлях зі школи до басейну на автобусі", pl: "Droga ze szkoły do basenu autobusem" }, time: "12:20 - 12:45" },
     { number: 4, subject: { ua: "Виховання фізичне (басейн)", pl: "Wychowanie fizyczne (basen)" }, time: "13:00 - 14:00" },
