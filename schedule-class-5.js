b4f5e0dced0919678c43a4c305ccbf1dda0bd27b
VikaVikaK <112518114+VikaVikaK@users.noreply.github.com>
2026-03-12 14:08:41 +0200

    Update schedule-class-5.js

--- schedule-class-5.js
+++ schedule-class-5.js
@@ -52,16 +52,20 @@ const schedule = {
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Українська мова", pl: "Język ukraiński" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
-    { number: 6, subject: { ua: "Українська література", pl: "Literatura ukraińska" }, time: "13:55 - 14:40" }
+    { number: 6, subject: { ua: "Українська література", pl: "Literatura ukraińska" }, time: "13:55 - 14:40" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "14:40 - 14:50" },
+    { number: 7, subject: { ua: "Факультатив: Арт-терапія", pl: "Fakultet: Arteterapia" }, time: "14:50 - 15:35" },
+    { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "15:35 - 15:45" },
+    { number: 8, subject: { ua: "Факультатив: Арт-терапія", pl: "Fakultet: Arteterapia" }, time: "15:45 - 16:30" }
   ],
   Thursday: [
-    { number: 1, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "9:00 - 9:45" },
+    { number: 1, subject: { ua: "Plastyka /Technika", pl: "Plastyka /Technika" }, time: "9:00 - 9:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "9:45 - 9:55" },
     { number: 2, subject: { ua: "Польська мова", pl: "Język polski" }, time: "9:55 - 10:40" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "10:40 - 10:50" },
     { number: 3, subject: { ua: "Досліджуємо історію та суспільство", pl: "Badamy historię i społeczeństwo" }, time: "10:50 - 11:35" },
     { number: " ", subject: { ua: "Обід 1-4 класи", pl: "Obiad klasy 1-4" }, time: "11:35 - 11:55" },
-    { number: 4, subject: { ua: "Plastyka /Technika", pl: "Plastyka /Technika" }, time: "11:55 - 12:40" },
+    { number: 4, subject: { ua: "Przyroda", pl: "Przyroda" }, time: "11:55 - 12:40" },
     { number: " ", subject: { ua: "Обід 5-6 класи", pl: "Obiad klasy 5-6" }, time: "12:40 - 13:00" },
     { number: 5, subject: { ua: "Англійська мова", pl: "Język angielski" }, time: "13:00 - 13:45" },
     { number: " ", subject: { ua: "Перерва", pl: "Przerwa" }, time: "13:45 - 13:55" },
